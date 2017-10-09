7b2bdee439155f0a401ad228f40d82554ae4cdef
Tiurin Sergey <aser4giam@yandex.ru>
2017-10-09 16:22:17 +0300

    PHP example

---
+++ PHP/example.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created
+ * User: TSN
+ * Date: 08.10.2017
+ * Time: 23:09
+ */
+
+class Fapi
+{
+
+    public static function getObjectList($number, $ui) {
+        $curl = curl_init(); // Инициализируем запрос
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'http://fapi.iisis.ru/fapi/v2/analogList?n='.$number.'&ui=' . ui, // Полный адрес метода
+            CURLOPT_RETURNTRANSFER => true, // Возвращать ответ
+            CURLOPT_POST => false,
+        ));
+        $response = curl_exec($curl); // Выполненяем запрос
+
+        $response = json_decode($response, false); // Декодируем из JSON
+        curl_close($curl); // Закрываем соединение
+        return $response; // Возвращаем ответ
+    }
+
+
+}
+
+ $result =  Fapi::getObjectList('w753');
+
+foreach ($result->analogList->a as  $row){
+    echo '<pre>';
+    echo "PRODUCT: " . $result->manufacturerList->mf[$row->mfi]->ds ." / " . $row->ns . " / ". $result->productList->p[$row->pi]->d . " ANALOG: ". $result->manufacturerList->mf[$row->mfai]->ds . " / " . $row->nsa . " / " . $result->productList->p[$row->pai]->d;
+    echo '</pre>';
+}
